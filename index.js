95861ade63d65109f5bcbb8f744b872acae192d9
Wonuola Alonge <57100593+wonuola-y@users.noreply.github.com>
2022-07-28 16:17:36 +0100

    side nav bar

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const button = document.querySelector('button')
+const button = document.querySelector('.dark')
 const paragraph = document.querySelector('p')
+const arrow = document.querySelectorAll('i')
+const list = document.querySelectorAll('.list')
+const arr = document.querySelectorAll('.arr')
+const cancel = document.querySelector('.close')
+const open = document.querySelector('.open')
+const nav = document.querySelector('.all')
+const navbar = document.querySelector('nav')
+const container = document.querySelector('.container')
 let every = document.body
 button.addEventListener('click', ()=>{
     every.classList.toggle('dark-mode')
-   
+   nav.style.backgroundColor = 'rgb(47, 48, 48)'
+   nav.style.color = 'white'
+   navbar.style.backgroundColor = 'white'
+   navbar.style.margin = '0'
     console.log('dark mode!!!')
 })
 if (every.classList.contains('dark-mode')) {
@@ -12,3 +23,27 @@ if (every.classList.contains('dark-mode')) {
 } else {
     paragraph.style.color = 'gray';
 }
+for (let i = 0; i < arrow.length; i++) {
+arrow[i].addEventListener('click', ()=>{
+       list[i].classList.toggle('show')
+       arr[i].classList.toggle('.turn')
+       console.log('kayy');
+})
+
+}
+
+open.addEventListener('click', ()=>{
+    nav.style.display = 'block';
+    nav.style.opacity = '1';
+    container.style.opacity = '0.4';
+    nav.style.opacity = '1';
+    nav.style.zIndex = '1';
+    open.style.transition = '2s';
+    })
+
+cancel.addEventListener('click', ()=>{
+nav.style.display = 'none';
+container.style.opacity = '1';
+container.style.opacity = '1';
+
+})
